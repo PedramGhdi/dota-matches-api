5328b8559b5448660071fb570ef0fe3cb00bc25a
Pedram Ghaderi <ghaderipedram16@yahoo.com>
2025-03-14 17:46:02 +0700

    Fix Error

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,20 +1,24 @@
 import { config } from "dotenv"
+import { execSync } from "child_process"
 import { defineConfig } from "tsup"
 
 config()
 
+const gitSha = execSync("git rev-parse --short HEAD").toString().trim()
+
 export default defineConfig({
   entry: ["src/index.ts"],
-  outDir: "dist",
 
   define: {
     "import.meta.env.MODE": JSON.stringify(
       process.env.NODE_ENV === "development" ? "development" : "production",
     ),
+    GIT_SHA: JSON.stringify(gitSha),
   },
 
   esbuildOptions: (options) => {
     options.supported = {
+      // For better performance: https://github.com/evanw/esbuild/issues/951
       "object-rest-spread": false,
     }
   },
